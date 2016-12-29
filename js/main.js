4352e29551866a2c867a320b3ec3f2ff8f54270b
JKAUSHALYA <jayangak@wso2.com>
2016-12-30 02:08:48 +0530

    Added new functionality.

--- js/main.js
+++ js/main.js
@@ -3,21 +3,30 @@
  */
 
 $(document).ready(function () {
+    $('#waiting-for-feed').show();
     $.get("https://protected-eyrie-93269.herokuapp.com/get-feed", function (data) {
         if (data == null) {
             console.error("No data received from server.")
             return;
         }
-
-        $('#rss-medium-title').text(data.title);
-
+        $('#waiting-for-feed').hide();
+        $('#rss-title').text(data.title);
         var feedEntries = data.feedEntries;
         if (feedEntries != null) {
-            var mediumDiv = $('#rss-medium');
-            feedEntries.forEach(function (feedEntry) {
-                mediumDiv.append('<div id="rss-medium-entry-title">' + feedEntry.title + '</div>');
-                mediumDiv.append(feedEntry.description);
-            });
+            var feedCount = feedEntries.length;
+            var i = 0;
+            setInterval(function () {
+                if (i === feedCount) {
+                    i = 0;
+                }
+                var rssFeedDiv = $('#rss-feed');
+                rssFeedDiv.hide();
+                rssFeedDiv.empty();
+                rssFeedDiv.append('<div id="rss-entry-title">' + feedEntries[i].title + '</div>');
+                rssFeedDiv.append('<div id="rss-entry">' + feedEntries[i].description + '</div>');
+                i++;
+                rssFeedDiv.slideDown();
+            }, 5000);
         }
     });
 });
